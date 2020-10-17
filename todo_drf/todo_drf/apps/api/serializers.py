3e7326706e1a772974fc9fbb7d9c45f7180e4f94
Charles Ryu <charlesryu@Charless-MacBook-Pro.local>
2020-10-17 09:49:30 -0700

    imprted serializer, api_views and response

---
+++ todo_drf/todo_drf/apps/api/serializers.py
@@ -0,0 +1,17 @@
+''' we added serializers to view a specific data in our models. 
+    Therefore, I imported serializers from rest frame work'''
+from rest_framework import serializers
+
+''' also imported the models from model '''
+from .models import *
+
+
+class TaskSerializer(serializers.ModelSerializer):
+  ''' for our sake of using the same name as model Task,  
+      I gave a name as TaskSerializer. Also, in order for this class to work
+      we need two attributes in the meta class, the model that we want, 
+      and how many fields we want to show.'''
+      
+  class Meta:
+    model = Task
+    field = '__all__'
--- todo_drf/todo_drf/apps/api/views.py
+++ todo_drf/todo_drf/apps/api/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+
+''' imports api views from frame work decorator '''
+from rest_framework.decorators import api_view 
+
+''' imports response from frame work response '''
+from rest_framework.response import Response
+
+''' we need to import serializers to use it in this view. '''
+from .serializers import TaskSerializer
 # Create your views here.
+
+''' this would allow me to use json data becuase we imported the api_views and the response '''
+@api_view(['GET'])
+def apiOverview(request):
+  api_urls = {
+    'List': '/task-list/', 
+    'Detail View': '/task-detail/<str:pk>', 
+    'Create': '/task-create/', 
+    'Update': '/task-update/<str:pk>/', 
+    'Delete': '/task-delete/<str:pk>/',
+
+  }
+  return JsonResponse(api_urls)
+
+  ''' Just so that we can see all of the datas in our base
+      create a function '''
